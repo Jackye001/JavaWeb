4e7437ce2a8c17957413561ce025448b3a69e8d3
yeck <jackye001@foxmail.com>
2020-10-14 23:45:21 +0800

    add Test.java ---yeck

--- Test.java
+++ Test.java
@@ -5,5 +5,6 @@ class Test{
     public static void main(String[] args) {
         System.out.println("this is not your people");
         System.out.println("this is the  second project");
+        System.out.println("this is the three priject");
     }
 }
