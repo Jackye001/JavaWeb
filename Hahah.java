b18d20ff12f3a8c5a31ea5fd8967c0cc3975b66b
yeck <jackye001@foxmail.com>
2020-10-28 22:58:41 +0800

    haha.java

--- Hahah.java
+++ Hahah.java
@@ -2,5 +2,6 @@ class Hahah{
     public static void main(String[] args) {
         System.out.println("this is another things");
         //“this is test”
+        System.out.println("this is my life");
     }
 }
